ffbd08620fe9b0292e72ca348ecc76bfb98c7bdd
sravan <57430307+sravan1946@users.noreply.github.com>
2022-11-10 16:32:59 +0530

    13 to 16

--- testing.py
+++ testing.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
 try:
     while True:
-        GPIO.output(13, GPIO.HIGH)
+        GPIO.output(16, GPIO.HIGH)
         sleep(1)
-        GPIO.output(13, GPIO.LOW)
+        GPIO.output(16, GPIO.LOW)
         sleep(1)
 finally:
     GPIO.cleanup()
