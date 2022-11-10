70fd03d09e166446db6a8e215a2eb1da73e79a58
sravan <57430307+sravan1946@users.noreply.github.com>
2022-11-10 15:04:12 +0530

    add try except

--- testing.py
+++ testing.py
@@ -3,8 +3,11 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT, initial=GPIO.LOW)
-GPIO.output(13, GPIO.HIGH)
-sleep(1)
-GPIO.output(13, GPIO.LOW)
-sleep(1)
-GPIO.cleanup()
+try:
+    while True:
+        GPIO.output(13, GPIO.HIGH)
+        sleep(1)
+        GPIO.output(13, GPIO.LOW)
+        sleep(1)
+finally:
+    GPIO.cleanup()
